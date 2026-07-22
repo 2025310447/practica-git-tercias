e62f358d8325e8c2485f7c3be1d7ed410dad42a4
Osvaldo Baltazar Luna <2025310099@uteq.edu.mx>
2026-07-21 19:15:27 -0600

    Cambio C

--- matematicas.py
+++ matematicas.py
@@ -1,6 +1,6 @@
 # Proyecto Colaborativo - Tercia
 numeros = [12, 45, 7, 23, 64, 10, 89, 3]
-print("Resultados del analisis de Estudiante B:")
+print("Resultados del analisis de Estudiante C:")
 # Desarrollado por Osvaldo - Promedio
 promedio = sum(numeros) / len(numeros)
 print(f"El promedio de la lista es: {promedio}")
